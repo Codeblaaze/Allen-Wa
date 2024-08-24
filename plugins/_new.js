6a4baa2697f2e0782e16943918d8a6724d55791f
Gimantha Dishan <gimanthadilshan64@gmail.com>
2024-08-25 00:10:30 +0530

    Add files via upload

--- plugins/_new.js
+++ plugins/_new.js
@@ -13,7 +13,7 @@ let {
  const cheerio = require("cheerio");
  smd(
   {
-    pattern: "channel",
+    pattern: "website",
     desc: "To check ping",
     react: "🌐",
     category: "user",
@@ -32,7 +32,7 @@ let {
 );
 smd(
   {
-    pattern: "wagroup",
+    pattern: "beautyai",
     desc: "TO GET SUPPORT NUMBER OF Hamza",
     react: "🌐",
     category: "user",
@@ -254,7 +254,7 @@ smd(
    }
  });
  smd({
-   cmdname: "bytemd",
+   cmdname: "allenbot",
    category: "misc",
    filename: __filename,
    info: "get counting for total features!"
@@ -281,7 +281,7 @@ smd(
          edit: _0x2d7cf6
        }, "asta", _0x4e7c63);
      } catch (_0x28ce7e) {}
-     let _0x50f17a = " *乂 BYTE-MD COMMANDS LIST*\n\n\n  ◦ _Total Features ➪ " + _0x4cf8ed + "_\n  \n*◦ LIST DOWN THE FEATURES*\n\n      _Commands ➪ " + Object.values(_0x4de967.commands).filter(_0x54d4bf => _0x54d4bf.pattern).length + "_\n      _Msg Listener ➪ " + Object.values(_0x4de967.commands).filter(_0x2376a3 => _0x2376a3.on).length + "_\n      _Call Listener ➪ " + Object.values(_0x4de967.commands).filter(_0x54a19b => _0x54a19b.call).length + "_\n      _Group Listener ➪ " + Object.values(_0x4de967.commands).filter(_0x35381c => _0x35381c.group).length + "_\n  \n\n" + Config.caption;
+     let _0x50f17a = " *乂 Allen Bot V2 COMMANDS LIST*\n\n\n  ◦ _Total Features ➪ " + _0x4cf8ed + "_\n  \n*◦ LIST DOWN THE FEATURES*\n\n      _Commands ➪ " + Object.values(_0x4de967.commands).filter(_0x54d4bf => _0x54d4bf.pattern).length + "_\n      _Msg Listener ➪ " + Object.values(_0x4de967.commands).filter(_0x2376a3 => _0x2376a3.on).length + "_\n      _Call Listener ➪ " + Object.values(_0x4de967.commands).filter(_0x54a19b => _0x54a19b.call).length + "_\n      _Group Listener ➪ " + Object.values(_0x4de967.commands).filter(_0x35381c => _0x35381c.group).length + "_\n  \n\n" + Config.caption;
      await _0x4e7c63.bot.relayMessage(_0x4e7c63.chat, {
        requestPaymentMessage: {
          currencyCodeIso4217: "NG",
