00db40483489e7277ec5961d0128dc80a96bfdab
Gimantha Dishan <gimanthadilshan64@gmail.com>
2024-09-12 10:31:04 +0530

    Add files via upload

--- plugins/cat.js
+++ plugins/cat.js
@@ -3,7 +3,7 @@ const { smd } = require("../lib");
 smd(
   {
     cmdname: "china",
-    desc: "Send Images of randome Cats!",
+    desc: "Send Images of randome Chinese girl",
     type: "fun",
     filename: __filename,
   },
@@ -17,6 +17,26 @@ smd(
       );
     } catch (e) {
       m.error(`${e}\n\nCommand: china`, e, false);
+    }
+  }
+);
+smd(
+  {
+    cmdname: "thai",
+    desc: "Send Images of random thai girl",
+    type: "fun",
+    filename: __filename,
+  },
+  async (m) => {
+    try {
+      await m.send(
+        "https://widipe.com/thailand",
+        { caption: "*hellooo*" },
+        "img",
+        m
+      );
+    } catch (e) {
+      m.error(`${e}\n\nCommand: thai`, e, false);
     }
   }
 );
